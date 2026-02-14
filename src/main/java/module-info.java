f5e48b41fc9f6c4715a2eddcb61ad18f52e31d17
AirCraft009 <coconraupe@gmail.com>
2026-02-14 10:44:17 +0100

    initial with JavaFx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.mxnik.forcechess {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.mxnik.forcechess to javafx.fxml;
+    exports org.mxnik.forcechess;
+}
---
+++ src/main/java/org/mxnik/forcechess/Launcher.java
@@ -0,0 +1,9 @@
+package org.mxnik.forcechess;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
