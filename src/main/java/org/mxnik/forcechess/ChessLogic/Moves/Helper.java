cf222d31219abef8fcffa59ab51991657371e146
AirCraft009 <coconraupe@gmail.com>
2026-02-15 19:56:04 +0100

    Helper for calculating positions

---
+++ src/main/java/org/mxnik/forcechess/ChessLogic/Moves/Helper.java
@@ -0,0 +1,49 @@
+package org.mxnik.forcechess.ChessLogic.Moves;
+
+import org.mxnik.forcechess.ChessLogic.Board;
+
+public class Helper {
+
+    /**
+     * get the row of a position
+     */
+    public static int getRow(int pos){
+        return pos / Board.sideLen;
+    }
+
+    /***
+     * get the column of a position
+     */
+    public static int getCol(int pos){
+        return pos % Board.sideLen;
+    }
+
+    /**
+     * get the distance to the left border;
+     */
+    public static int distanceLeftB(int pos){
+        return (distanceRightB(pos) - Board.sideLen) + 1;
+    }
+
+    /**
+     * get the distance to the right border;
+     */
+    public static int distanceRightB(int pos){
+        return Board.sideLen - (getCol(pos) + 1);
+    }
+
+    /**
+     * get the distance to the top of the board;
+     */
+    public static int distanceTopB(int pos){
+        return (Board.sideLen - getRow(pos)) * Board.sideLen;
+    }
+
+
+    /**
+     * get the distance to the bottom of the board;
+     */
+    public static int distanceBottomB(int pos){
+        return getRow(pos) * Board.sideLen;
+    }
+}
