9377593432c01562460015a3d069af67dd9809ad
AirCraft009 <coconraupe@gmail.com>
2026-02-15 20:42:05 +0100

    added diagonal movement offsets

--- src/main/java/org/mxnik/forcechess/ChessLogic/Moves/MoveOffsets.java
+++ src/main/java/org/mxnik/forcechess/ChessLogic/Moves/MoveOffsets.java
@@ -3,7 +3,14 @@ package org.mxnik.forcechess.ChessLogic.Moves;
 import org.mxnik.forcechess.ChessLogic.Board;
 
 public enum MoveOffsets {
-    UP(Board.sideLen), DOWN(-Board.sideLen), LEFT(-1), RIGHT(1);
+    UP(Board.sideLen),
+    DOWN(-Board.sideLen),
+    LEFT(-1),
+    RIGHT(1),
+    UP_R(Board.sideLen+1),
+    UP_L(Board.sideLen-1),
+    DOWN_R(-Board.sideLen+1),
+    DOWN_L(-Board.sideLen-1);
 
     int offset;
     MoveOffsets(int offset) {
