86ddbedb065e7dd54ca68bdd45fbf6495ce3a48f
SVE240406 <sve240406@spengergasse.at>
2026-02-27 22:31:30 +0100

    Pictures of pieces on the board

--- src/main/java/org/mxnik/forcechess/UI/Constants.java
+++ src/main/java/org/mxnik/forcechess/UI/Constants.java
@@ -1,17 +1,17 @@
 package org.mxnik.forcechess.UI;
 
 import javafx.geometry.Rectangle2D;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.Scene;
 import javafx.stage.Screen;
 
-import java.awt.*;
+import javafx.scene.paint.*;
 
 public class Constants {
     final int BoardSize;
     final int WidthStart;
     final int BlockS;
-    final Color DarkColor = Color.BLACK;
-    final Color WhiteColor = Color.WHITE;
+    final Paint DarkColor = Color.DARKBLUE;
+    final Paint WhiteColor = Color.WHITE;
     final Rectangle2D bounds;
 
 
@@ -20,6 +20,15 @@ public class Constants {
         bounds = screen.getVisualBounds();
         BoardSize = (int) bounds.getHeight();
         int middle = (int) bounds.getWidth() / 2;
+        WidthStart = middle - BoardSize / 2;
+        BlockS = BoardSize / sideLen;
+    }
+
+    public Constants(int sideLen, Scene scene){
+        Screen screen = Screen.getPrimary();
+        bounds = screen.getVisualBounds();
+        BoardSize = (int) scene.getHeight();
+        int middle = (int) scene.getWidth() / 2;
         WidthStart = middle - BoardSize / 2;
         BlockS = BoardSize / sideLen;
     }
