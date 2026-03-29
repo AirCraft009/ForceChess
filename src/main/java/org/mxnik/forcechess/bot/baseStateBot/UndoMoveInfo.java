4c3fea6c0ed1b57cd8a195806c57960b96f94c6b
AirCraft009 <coconraupe@gmail.com>
2026-03-29 23:45:28 +0200

    removed bitboards from UndoMoveInfo as they're no longer Bitboards but longs

--- src/main/java/org/mxnik/forcechess/bot/baseStateBot/UndoMoveInfo.java
+++ src/main/java/org/mxnik/forcechess/bot/baseStateBot/UndoMoveInfo.java
@@ -8,9 +8,7 @@ import org.mxnik.forcechess.Util.Bitboard;
  * @param from origin position
  * @param to current Piece position
  * @param flags type of move (Generic, Castle, enPassent, etc.)
- * @param pieceBitboard Bitboard of the moved piece
- * @param secondaryBitboard secondary Bitboard (WRook after castle, BQueen after QueenTrade)
  *
  */
-public record UndoMoveInfo(Integer from, Integer to, Integer flags, long pieceBitboard, long secondaryBitboard) {
+public record UndoMoveInfo(Integer from, Integer to, Integer flags) {
 }
