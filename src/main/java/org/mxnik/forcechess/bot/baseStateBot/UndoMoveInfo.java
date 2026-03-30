ecf14ad25f0e048c48a10ecc380cbd2b15360d63
AirCraft009 <coconraupe@gmail.com>
2026-03-30 23:42:21 +0200

    Changed UndoMoveInfo to be repr. by an int similarly to move

--- src/main/java/org/mxnik/forcechess/bot/baseStateBot/UndoMoveInfo.java
+++ src/main/java/org/mxnik/forcechess/bot/baseStateBot/UndoMoveInfo.java
@@ -1,14 +1,26 @@
 package org.mxnik.forcechess.bot.baseStateBot;
 
-import org.mxnik.forcechess.Util.Bitboard;
 
-/**
- * Info for unmaking a move
- *
- * @param from origin position
- * @param to current Piece position
- * @param flags type of move (Generic, Castle, enPassent, etc.)
- *
- */
-public record UndoMoveInfo(Integer from, Integer to, Integer flags) {
+// Info to unmake a move packed into an int
+// bits 0  - 19 : Move (see bit desc. in Move class)
+// bits 20 - 23 : TakenPiece (Type and Color of the taken piece -> Empty Piece if none were taken
+public final class UndoMoveInfo {
+
+    public static int of(int move, int takenPiece){
+        return move | takenPiece << 20;
+    }
+
+    public static int move(int info){
+        return info & MOVE_MASK;
+    }
+
+    public static int takenPiece(int info){
+        return (info >> TAKEN_PIECE_SHIFT) & TAKEN_PIECE_MASK;
+    }
+
+    private final static int MOVE_MASK = 0x7FFFF;
+    private final static int TAKEN_PIECE_MASK = 0xF;
+
+    private final static int TAKEN_PIECE_SHIFT = 20;
+
 }
