92c9ecc5830d9cc976cab460f63f234171d32005
AirCraft009 <coconraupe@gmail.com>
2026-04-17 23:01:59 +0200

    finalized training and added helper methods for checkpoints

--- src/main/java/org/mxnik/forcechess/engine/network/Train.java
+++ src/main/java/org/mxnik/forcechess/engine/network/Train.java
@@ -8,18 +8,18 @@ import org.mxnik.forcechess.engine.Pos.PositionEncoder;
 import org.mxnik.forcechess.engine.bot.ChessBot;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.MultiDataSet;
-import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Train {
-    private ChessBot bot;
-    private AlphaNet network;
-    private String fullPath;
+    private final ChessBot bot;
+    private final AlphaNet network;
+    private final String fullPath;
+    private final String fileName;
     private int checkPointC = 0;
-    public final static String basePath = "boardsNBots/bots/";
+    public final static String BASE_PATH = "boardsNBots/bots/networks/";
 
     /**
      * Read the configured AI-model from the file specified (no file ending)
@@ -29,7 +29,8 @@ public class Train {
     }
 
     private Train(String fileName, boolean read) throws IOException {
-        fullPath = basePath + fileName;
+        fullPath = BASE_PATH + fileName;
+        this.fileName = fileName;
         if (!read) {
             network = new AlphaNet(NetworkConfig.buildNet());
             bot = new ChessBot(network);
@@ -47,7 +48,8 @@ public class Train {
      * creates a Train instances with the given network that will be saved to fileName
      */
     public Train(AlphaNet net, String fileName) {
-        fullPath = basePath + fileName;
+        fullPath = BASE_PATH + fileName;
+        this.fileName = fileName;
         network = net;
         bot = new ChessBot(net);
     }
@@ -58,16 +60,22 @@ public class Train {
         checkPointC++;
     }
 
+    public void saveNet() throws IOException {
+        ModelSerializer.writeModel(network.getModel(), new File(fullPath + ".zip"), true);
+    }
+
     /**
-     * trains a NeuralNetwork
+     * plays games against itself and returns a SampleBuffer for training
+     *
      * @param size amount of moves played in total over all games
      * @param moveDepth how often the MCTS-Loop is run for each move
      */
     public SampleBuffer selfPlayGames(int size, int moveDepth) {
-        SampleBuffer buffer = new SampleBuffer(size);
+        SampleBuffer buffer = new SampleBuffer(size, fileName + "_buffer");
         for (int i = 0; i < size; i++) {
-            bot.selfPlayGame(moveDepth, buffer);
+            i = bot.selfPlayGame(moveDepth, i, size, buffer);
         }
+        System.out.println("Finished self play");
         return buffer;
     }
 
@@ -86,9 +94,9 @@ public class Train {
 
         for (int i = 0; i < batchSize; i++) {
             TrainingSample s = buffer.sample();
-            inputs.putSlice(i, Nd4j.create(s.tensor(), new int[]{PositionEncoder.PLANES, PositionEncoder.SIZE, PositionEncoder.SIZE}, 'c'));
-            piTargets.putRow(i, Nd4j.create(s.pi()));
-            zTargets.putRow(i, Nd4j.create(new float[]{s.z()}));
+            inputs.putSlice(i, Nd4j.create(s.tensor, new int[]{PositionEncoder.PLANES, PositionEncoder.SIZE, PositionEncoder.SIZE}, 'c'));
+            piTargets.putRow(i, Nd4j.create(s.pi));
+            zTargets.putRow(i, Nd4j.create(new float[]{s.z}));
         }
 
         network.getModel().fit(new MultiDataSet(
@@ -102,16 +110,42 @@ public class Train {
     }
 
     /**
+     *
+     * Trains the AI-model after playing selfPlayGames
+     *
+     * @param batchSize how big the batch is that the training is used on
+     * @param SampleBufferSize amount of positions in the SampleBuffer from the self-play
+     * @param n how many MCTS evals per move
+     * @param epoch how many times the net will be trained (uses the same sample buffer)
      * @param checkPoint how many batches have to be played till a checkpoint is saved <p></p>
      *                   - checkpoints are set as filename_n_checkPoint.zip
      */
-    public void train(int checkPoint){
-        SampleBuffer b = selfPlayGames(1, 300);
-        trainFromBuffer(10, b);
+    public void train(int batchSize, int SampleBufferSize, int n, int epoch, int checkPoint) throws IOException {
+        SampleBuffer b = selfPlayGames(SampleBufferSize, n);
+        train(batchSize, b, epoch, checkPoint);
     }
 
-    public static void main(String[] args) {
-        Train train = new Train(new AlphaNet(NetworkConfig.buildNet()), "aa");
-        train.train(10);
+    /**
+     * Trains the AI-model with a given SampleBuffer
+     *
+     * @param batchSize how big the batch is that the training is used on
+     * @param buffer the buffer used to train with
+     * @param epoch how many times the net will be trained (uses the same sample buffer)
+     * @param checkPoint how many batches have to be played till a checkpoint is saved <p></p>
+     *                   - checkpoints are set as filename_n_checkPoint.zip
+     */
+    public void train(int batchSize, SampleBuffer buffer, int epoch, int checkPoint) throws IOException {
+        for (int i = 1; i < epoch + 1; i++) {
+            trainFromBuffer(batchSize, buffer);
+            if(i % checkPoint == 0){
+                saveCheckPoint();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Train train = new Train(new AlphaNet(NetworkConfig.buildNet()), "D300_T1");
+        train.train(32, 1600, 300, 100, 10);
     }
 }
