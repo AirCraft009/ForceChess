30dc6e2df18ca91f6a1859e71f651950fd549a65
AirCraft009 <coconraupe@gmail.com>
2026-04-17 23:02:11 +0200

    turned TrainingSample into a class

--- src/main/java/org/mxnik/forcechess/engine/network/TrainingSample.java
+++ src/main/java/org/mxnik/forcechess/engine/network/TrainingSample.java
@@ -1,4 +1,15 @@
 package org.mxnik.forcechess.engine.network;
 
-public record TrainingSample(float[] tensor, float[] pi, float z) {
+
+public final class TrainingSample {
+    public final float[] tensor;
+    public final float[] pi;
+    public  float z;
+
+    public TrainingSample(float[] tensor, float[] pi, float z){
+        this.tensor = tensor;
+        this.pi = pi;
+        this.z = z;
+    }
+
 }
