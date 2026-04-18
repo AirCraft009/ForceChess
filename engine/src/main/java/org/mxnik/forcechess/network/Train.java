b2649f866901dbee70a3ffdba553916d608f38c9
AirCraft009 <coconraupe@gmail.com>
2026-04-18 21:43:03 +0200

    fixed position not resetting after 1 game in self-play game-loop

--- engine/src/main/java/org/mxnik/forcechess/network/Train.java
+++ engine/src/main/java/org/mxnik/forcechess/network/Train.java
@@ -1,11 +1,14 @@
 package org.mxnik.forcechess.network;
 
 
+import com.sun.jna.platform.unix.X11;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.util.ModelSerializer;
 import org.mxnik.forcechess.Pos.Move;
 import org.mxnik.forcechess.Pos.PositionEncoder;
+import org.mxnik.forcechess.bot.BatchChessBot;
 import org.mxnik.forcechess.bot.ChessBot;
+import org.mxnik.forcechess.bot.Evaluator;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.MultiDataSet;
 import org.nd4j.linalg.factory.Nd4j;
@@ -54,6 +57,21 @@ public class Train {
         bot = new ChessBot(net);
     }
 
+    /**
+     * creates a Train instances with the given ChessBot
+     */
+    public Train(ChessBot bot, String fileName) {
+        fullPath = BASE_PATH + fileName;
+        this.fileName = fileName;
+
+        Evaluator e = bot.getEvaluator();
+        if(e.getClass() != AlphaNet.class){
+            throw new IllegalArgumentException("Only Bots with an AlphaNet can be passed to the train method");
+        }
+        this.network = (AlphaNet) e;
+        this.bot = bot;
+    }
+
     public void saveCheckPoint() throws IOException {
         String checkPath = fullPath + "_" + checkPointC + "_checkPoint.zip";
         ModelSerializer.writeModel(network.getModel(), new File(checkPath), true);
@@ -65,17 +83,18 @@ public class Train {
     }
 
     /**
-     * plays games against itself and returns a SampleBuffer for training
+     * plays games against itself and fills a SampleBuffer for training
      *
      * @param size amount of moves played in total over all games
      * @param moveDepth how often the MCTS-Loop is run for each move
      */
-    public SampleBuffer selfPlayGames(int size, int intermediate, int moveDepth, SampleBuffer buffer) {
+    public void selfPlayGames(int size, int moveDepth, SampleBuffer buffer) {
         for (int i = 0; i < size; i++) {
-            i = bot.selfPlayGame(moveDepth, i, size, intermediate, buffer);
+            i = bot.selfPlayGame(moveDepth, i, size, buffer);
+            bot.resetCore();
+            bot.setPos(PositionEncoder.Position.StartingPosition());
         }
         System.out.println("Finished self play");
-        return buffer;
     }
 
     private void trainFromBuffer(int batchSize, SampleBuffer buffer){
@@ -103,6 +122,10 @@ public class Train {
                 new INDArray[] {piTargets, zTargets}
         ));
 
+        inputs.close();
+        piTargets.close();
+        zTargets.close();
+
         double scores = network.getModel().score();
         System.out.printf("policy loss: %.4f\n", scores);
     }
@@ -112,36 +135,34 @@ public class Train {
      * Trains the AI-model after playing selfPlayGames
      *
      * @param batchSize how big the batch is that the training is used on
-     * @param SampleBufferSize amount of positions in the SampleBuffer from the self-play
+     * @param sampleBufferSize amount of positions in the SampleBuffer from the self-play
      * @param n how many MCTS evals per move
      * @param epoch how many times the net will be trained (uses the same sample buffer)
      * @param checkPoint how many batches have to be played till a checkpoint is saved <p></p>
      *                   - checkpoints are set as filename_n_checkPoint.zip
      */
-    public void train(int batchSize, int SampleBufferSize, int n, int epoch, int sampleCheckPoint, int checkPoint) throws IOException {
-        SampleBuffer buffer = new SampleBuffer(SampleBufferSize, fileName + "_buffer");
-        SampleBuffer b = selfPlayGames(SampleBufferSize, sampleCheckPoint, n, buffer);
-        train(batchSize, b, epoch, checkPoint);
+    public void train(int batchSize, int sampleBufferSize, int n, int epoch, int checkPoint, boolean saveBuffer) throws IOException {
+        SampleBuffer buffer = new SampleBuffer(sampleBufferSize, fileName + "_buffer");
+        train(batchSize, sampleBufferSize, n, epoch, checkPoint, buffer, saveBuffer);
     }
 
     /**
      * Trains the AI-model with a given SampleBuffer that is then expanded
      *
      * @param batchSize how big the batch is that the training is used on
-     * @param size how big the sample buffer should be in the end
+     * @param sampleBufferSize how big the sample buffer should be in the end
      * @param buffer the buffer used to train with
+     * @param saveBuffer should the buffer be saved after filling it
      * @param epoch how many times the net will be trained (uses the same sample buffer)
      * @param checkPoint how many batches have to be played till a checkpoint is saved <p></p>
      *                   - checkpoints are set as filename_n_checkPoint.zip
      */
-    public void train(int batchSize, int size, int n, int sampleCheckpoint, int epoch, int checkPoint, SampleBuffer buffer) throws IOException {
-        selfPlayGames(size, sampleCheckpoint, n, buffer);
-        for (int i = 1; i < epoch + 1; i++) {
-            trainFromBuffer(batchSize, buffer);
-            if(i % checkPoint == 0){
-                saveCheckPoint();
-            }
-        }
+    public void train(int batchSize, int sampleBufferSize, int n, int epoch, int checkPoint, SampleBuffer buffer, boolean saveBuffer) throws IOException {
+        selfPlayGames(sampleBufferSize, n, buffer);
+        if(saveBuffer)
+            buffer.writeSamples();
+
+        train(batchSize, buffer, epoch, checkPoint);
     }
 
     /**
@@ -164,8 +185,8 @@ public class Train {
 
 
     public static void main(String[] args) throws IOException {
-        Train train = new Train(new AlphaNet(NetworkConfig.buildNet()), "D300_D250_T0");
-        train.train(100, 10000, 300, 1000,1000, 100);
+        Train train = new Train(new BatchChessBot(new AlphaNet(NetworkConfig.buildNet())), "BatchedT1");
+        train.train(32, 5000, 400, 10000, 1000, true);
         train.saveNet();
     }
 }
