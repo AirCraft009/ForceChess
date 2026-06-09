704793d7eb84d48554877bdd032928ed12fc76f0
AirCraft009 <coconraupe@gmail.com>
2026-06-09 19:22:14 +0200

    added SuperMain

---
+++ user/src/main/java/org/mxnik/forcechess/main/SuperMain.java
@@ -0,0 +1,12 @@
+package org.mxnik.forcechess.main;
+
+import org.mxnik.forcechess.UI.ChessControllView.ChessApplication;
+
+public class SuperMain {
+
+
+    public static void main(String[] args) {
+        ChessApplication.main(args);
+
+    }
+}
